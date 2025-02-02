553f9ef7f66ca1f5ad7a6d7b6179dc190e230ab4
Andres060 <148551646+Andres060@users.noreply.github.com>
2025-02-02 15:41:38 -0500

    Septimo commit

--- archivo1.py
+++ archivo1.py
@@ -1,8 +1,9 @@
-print("Hola Señor/a")
-
 print("¿Cual es su nombre?")
 nombre = input()
+print(f"Hola señor/a {nombre}")
+
 x = int(input("Ingrese un numero: "))
 y = int(input("Ingrese otro numero: "))
 
 print("¿Que tabla de multiplicar quiere?")
+z = int(input("Numero de la tabla: "))
